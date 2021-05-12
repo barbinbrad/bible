0e735536a5a609d7bffe4e9ad843cdfa21a31440
barbinbrad <bradbarbin@protonmail.com>
2021-05-11 21:22:11 -0400

    improve chapter list component

--- build/build-chapters-pages.js
+++ build/build-chapters-pages.js
@@ -15,7 +15,7 @@ const VersesTable = require('../database/verses');
 
     for(chapter of chapters){      
 
-        if(chapter.name == 'Exodus'){
+        if(chapter.name == 'Numbers'){
             break;
         }
 
@@ -32,17 +32,19 @@ const VersesTable = require('../database/verses');
         html += '</head>';
         html += '<body>';
         html +=     '<div id="navigation">';
-        html +=         '<div id="search">';
-        html +=             '<div class="input">';
-        html +=                '<div class="icon">';
-        html +=                   '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">';
-        html +=                         '<path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>';
-        html +=                     '</svg>';
+        html +=         '<div>';
+        html +=             '<div id="search">';
+        html +=                 '<div class="input">';
+        html +=                     '<div class="icon">';
+        html +=                         '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">';
+        html +=                             '<path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>';
+        html +=                         '</svg>';
+        html +=                     '</div>';
+        // html +=                  '<autocomplete></autocomplete>';
         html +=                 '</div>';
-        html +=                 '<autocomplete></autocomplete>';
         html +=             '</div>';
+        html +=             `<chapter-slideout book="${chapter.name}"></chapter-slideout>`;
         html +=         '</div>';
-        html +=         '<chapter-slideout></chapter-slideout>';
         html +=     '</div>';
         html +=     '<div id="content">';
         html +=         `<h2>${chapter.name} ${chapter.number}</h2>`
@@ -64,42 +66,9 @@ const VersesTable = require('../database/verses');
         }
         html +=     '</div>'
 
-        html += '<script type="text/x-template" id="autocomplete">';
-        html +=     '<div>';
-        html +=         '<input type="text" placeholder="Books" id="acinput" name="acinput" @keyup="fetchData()" @keydown="setFocus" @focus="inputFocus(true)" @blur="inputFocus(false)" v-model="inputtext" class="form-control"/>';
-        html +=         '<list :fetchedData="autocompleteList"></list>'  
-        html +=     '</div>';
-        html += '</script>';
-    
-        html += '<script type="text/x-template" id="results-list">';
-        html +=     '<div class="results">';
-        html +=         '<div class="autolist" v-if="fetchedData.length>0">';
-        html +=             '<ul class="list-group">';
-        html +=                 '<li class="list-group-item" :class="checkSelected(index)" v-for="(dt, index) in fetchedData" @mousemove="mouseHover(index)" @mousedown="selectMe(index)">{{dt}}</li>';
-        html +=             '</ul>';
-        html +=         '</div>';
-        html +=     '</div>';
-        html += '</script>';
-
-        html += '<script id="chapter-slideout" type="text/x-template">';
-        html +=     '<div class="zap-slideout" :class="{ isOpen: isOpen }">';
-        html +=         '<div class="zap-slideout-opener" v-if="!isOpen" @click="open">';
-        html +=             '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">';
-        html +=                 '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>';
-        html +=             '</svg>';
-        html +=         '</div>';
-        html +=         '<ul class="zap-slideout-menu">';
-        html +=             '<li class="zap-slideout-closer" @click="close">';
-        html +=                 '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">';
-        html +=                     '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>';
-        html +=                 '</svg>';
-        html +=             '</li>';
-        html +=             '<li class="zap-slideout-menu-item" v-for="item in menu">{{item}}</li>';
-        html +=             '<li class="zap-slideout-menu-item--small" v-for="item in smallMenu">{{item}}</li>';
-        html +=         '</ul>';
-        html +=     '</div>';
-        html += '</script>';
-
+        html += utils.AutoComplete;    
+        html += utils.AutoCompleteResults;
+        html += utils.ChapterDrawer;
 
         html += '<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.0.2/vue.js"></script>';
         html += '<script src="https://cdnjs.cloudflare.com/ajax/libs/vuex/2.0.0/vuex.js"></script>';
--- build/utils.js
+++ build/utils.js
@@ -14,4 +14,57 @@ function addPlusSignToNormalLink(url){
 const LeftArrow  = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11.5 21.5" width="11.5" height="21.5"><title>Previous</title><path d="M10.75 0a.74.74 0 0 1 .53.22.75.75 0 0 1 0 1.06l-9.47 9.47 9.47 9.47a.75.75 0 1 1-1.06 1.06l-10-10a.75.75 0 0 1 0-1.06l10-10a.73.73 0 0 1 .53-.22z"></path></svg>';
 const RightArrow = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11.5 21.5" width="11.5" height="21.5"><title>Next</title><path d="M.75 21.5a.74.74 0 0 1-.53-.22.75.75 0 0 1 0-1.06l9.47-9.47L.22 1.28A.75.75 0 0 1 1.28.22l10 10a.75.75 0 0 1 0 1.06l-10 10a.74.74 0 0 1-.53.22z"></path></svg>';
 
-module.exports = {addPlusSignToNormalLink, LeftArrow, RightArrow};
+const AutoComplete = autocompleteTemplate(); 
+const AutoCompleteResults = autocompleteResultsTemplate();
+const ChapterDrawer = chapterDrawerTemplate();
+
+function autocompleteTemplate(){
+    html = '<script type="text/x-template" id="autocomplete">';
+    html +=     '<div>';
+    html +=         '<input type="text" placeholder="Books" id="acinput" name="acinput" @keyup="fetchData()" @keydown="setFocus" @focus="inputFocus(true)" @blur="inputFocus(false)" v-model="inputtext" class="form-control"/>';
+    html +=         '<list :fetchedData="autocompleteList"></list>'  
+    html +=     '</div>';
+    html += '</script>';
+
+    return html;
+}
+
+function autocompleteResultsTemplate(){
+    html = '<script type="text/x-template" id="results-list">';
+    html +=     '<div class="results">';
+    html +=         '<div class="autolist" v-if="fetchedData.length>0">';
+    html +=             '<ul class="list-group">';
+    html +=                 '<li class="list-group-item" :class="checkSelected(index)" v-for="(dt, index) in fetchedData" @mousemove="mouseHover(index)" @mousedown="selectMe(index)">{{dt}}</li>';
+    html +=             '</ul>';
+    html +=         '</div>';
+    html +=     '</div>';
+    html += '</script>';
+    
+    return html;
+}
+
+function chapterDrawerTemplate(){
+    html = '<script id="chapter-slideout" type="text/x-template">';
+    html +=     '<div class="zap-slideout" :class="{ isOpen: isOpen }">';
+    html +=         '<div class="zap-slideout-opener" v-if="!isOpen" @click="open">';
+    html +=             '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">';
+    html +=                 '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>';
+    html +=             '</svg>';
+    html +=         '</div>';
+    html +=         '<ul class="zap-slideout-menu">';
+    html +=             '<li class="zap-slideout-closer" @click="close">';
+    html +=                 '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">';
+    html +=                     '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>';
+    html +=                 '</svg>';
+    html +=             '</li>';
+    html +=             '<li class="zap-slideout-menu-item" v-for="book in bookChapters">';
+    html +=                 '<a :href="book.link">{{book.name}} {{book.number}}</a>';
+    html +=             '</li>';
+    html +=         '</ul>';
+    html +=     '</div>';
+    html += '</script>';
+
+    return html;
+}
+
+module.exports = {addPlusSignToNormalLink, LeftArrow, RightArrow, AutoComplete, AutoCompleteResults, ChapterDrawer};
