8fe7f3bed74968ceba2e6c32212da40e9aaef9e2
barbinbrad <bradbarbin@protonmail.com>
2021-05-18 19:56:35 -0400

    update service worker to be non-blocking

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -314,30 +314,6 @@ Vue.component('bookmark-dropdown', {
         this.bookmarkTitle = (pageTitle) ? pageTitle : '';
         this.bookmarkLink = (pageLink) ? pageLink : '';
     },
-    mounted: function() {
-        // TODO: understand lifecycle hooks
-        setTimeout(() => { 
-            // get all outbound links in the DOM
-            let links = [...document.querySelectorAll('a')];
-            let urls = new Set();
-
-            if (links.length == 0) {
-                return;
-            }
-            console.log(links);
-            caches.open(`minimal-bible-${serviceWorkerVersion}`).then(function(cache) {
-                // iterate over the links in the DOM
-                for (link of links) {
-                    // get the relative URL
-                    let url = new URL(link.href)
-                    urls.add(url.pathname);
-                }
-                // cache all outbound links
-                cache.addAll([...urls]);
-            });
-         }, 1000);
-        
-    },
     data() {
         return {
             bookmarkTitle: '',
@@ -410,6 +386,28 @@ new Vue({
 window.addEventListener('load', (event) => {
     const y = new URL(location).searchParams.get('y');
     window.scroll(0, y);
+
+    // TODO: understand lifecycle hooks
+    setTimeout(() => { 
+        // get all outbound links in the DOM
+        let links = [...document.querySelectorAll('a')];
+        let urls = new Set();
+
+        if (links.length == 0) {
+            return;
+        }
+        
+        caches.open(`minimal-bible-${serviceWorkerVersion}`).then(function(cache) {
+            // iterate over the links in the DOM
+            for (link of links) {
+                // get the relative URL
+                let url = new URL(link.href)
+                urls.add(url.pathname);
+            }
+            // cache all outbound links
+            cache.addAll([...urls]);
+        });
+     }, 1000);
 });
 
 
