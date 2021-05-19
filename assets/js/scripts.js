2f8b18909b89e6bce59a7444c5f7abef560fa410
barbinbrad <bradbarbin@protonmail.com>
2021-05-18 21:50:38 -0400

    revert

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -314,6 +314,27 @@ Vue.component('bookmark-dropdown', {
         this.bookmarkTitle = (pageTitle) ? pageTitle : '';
         this.bookmarkLink = (pageLink) ? pageLink : '';
     },
+    mounted: function() {
+        
+        // get all outbound links in the DOM
+        let links = [...document.querySelectorAll('a')];
+        let urls = [];
+
+        if (links.length == 0) {
+            return;
+        }
+        console.log(links);
+        caches.open(`minimal-bible-${serviceWorkerVersion}`).then(function(cache) {
+            // iterate over the links in the DOM
+            for (link of links) {
+                // get the relative URL
+                let url = new URL(link.href)
+                urls.push(url.pathname);
+            }
+            // cache all outbound links
+            cache.addAll(urls);
+        });
+    },
     data() {
         return {
             bookmarkTitle: '',
@@ -383,11 +404,11 @@ new Vue({
     store
 });
 
-
 window.addEventListener('load', (event) => {
     const y = new URL(location).searchParams.get('y');
     window.scroll(0, y);
 });
+
 
 
 /* 
@@ -409,26 +430,7 @@ if (development == false) {
             });
 
         navigator.serviceWorker.ready.then(function(registration) {
-            // get all outbound links in the DOM
-            let links = [...document.querySelectorAll('a')];
-            let urls = new Set();
-
-            if (links.length == 0) {
-                return;
-            }
-            
-            caches.open(`minimal-bible-${serviceWorkerVersion}`).then(function(cache) {
-                // iterate over the links in the DOM
-                for (link of links) {
-                    // get the relative URL
-                    let url = new URL(link.href)
-                    urls.add(url.pathname);
-                }
-                // cache all outbound links
-                cache.addAll([...urls]);
-            });
+            //console.log('Service Worker Ready');
         });
     }
-
-
 }
