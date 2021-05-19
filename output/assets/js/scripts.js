8a922fcebe51ad47e73cba31ef6d77451c9bea97
barbinbrad <bradbarbin@protonmail.com>
2021-05-18 21:45:12 -0400

    move service worker crawler to sw callback

--- output/assets/js/scripts.js
+++ output/assets/js/scripts.js
@@ -383,33 +383,11 @@ new Vue({
     store
 });
 
+
 window.addEventListener('load', (event) => {
     const y = new URL(location).searchParams.get('y');
     window.scroll(0, y);
-
-    // TODO: understand lifecycle hooks
-    setTimeout(() => { 
-        // get all outbound links in the DOM
-        let links = [...document.querySelectorAll('a')];
-        let urls = new Set();
-
-        if (links.length == 0) {
-            return;
-        }
-        
-        caches.open(`minimal-bible-${serviceWorkerVersion}`).then(function(cache) {
-            // iterate over the links in the DOM
-            for (link of links) {
-                // get the relative URL
-                let url = new URL(link.href)
-                urls.add(url.pathname);
-            }
-            // cache all outbound links
-            cache.addAll([...urls]);
-        });
-     }, 50);
 });
-
 
 
 /* 
@@ -431,7 +409,26 @@ if (development == false) {
             });
 
         navigator.serviceWorker.ready.then(function(registration) {
-            //console.log('Service Worker Ready');
+            // get all outbound links in the DOM
+            let links = [...document.querySelectorAll('a')];
+            let urls = new Set();
+
+            if (links.length == 0) {
+                return;
+            }
+            
+            caches.open(`minimal-bible-${serviceWorkerVersion}`).then(function(cache) {
+                // iterate over the links in the DOM
+                for (link of links) {
+                    // get the relative URL
+                    let url = new URL(link.href)
+                    urls.add(url.pathname);
+                }
+                // cache all outbound links
+                cache.addAll([...urls]);
+            });
         });
     }
+
+
 }
